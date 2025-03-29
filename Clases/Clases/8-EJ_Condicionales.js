68b5a0951353d7bb2f56534e892b4a248e1fbe5b
LUMA025 <luismanuel.rincon24@gmail.com>
2025-03-29 13:34:24 -0400

    Add files via upload

---
+++ Clases/Clases/8-EJ_Condicionales.js
@@ -0,0 +1,165 @@
+/* ---EJERCICIOS: Condicionales--- */
+
+// 1: imprime por consola tu nombre si una variable toma su valor
+
+
+
+// 2: imprime por consola un mensaje si el usuario y contrasena coincide con unos establecidos
+
+let name='luis rincon'
+let pass='24072004'
+if(name!='luis rincon'){
+
+console.log('el nombre de usuario es incorrecto')
+
+}else if(pass!='24072004'){
+
+console.log('la contrasena ingresada es incorrecta')
+
+}else{
+
+console.log('bienvenido')
+
+}
+
+// 3: verifica si un numero es positivo, negativo o 0 e imprime un mensaje
+
+let num=0
+
+if(num>=1){console.log('el numero es positivo')}else if(num<=-1){console.log('el numero es negativo')}else{console.log('el numero es 0')}
+
+// 4: verifica si una persona puede votar o no e identifica cuantos anos le faltan
+
+let age=10
+if(age>=18){
+    console.log('eres mayor de edad, puedes votar')
+}else if(age<18 && age>=1){
+    
+    age= 18 - age
+    console.log('eres menor de edad y te faltan: '+ age +' anos')
+
+}else{
+    console.log('la edad no es valida')}
+
+// 5: Usa el operador ternario para asignar el valor 'adulto' o 'menor' a una variable dependiendo de la edad
+
+const ageover = age >=18 ? 'adulto' : 'menor'
+console.log(ageover)
+
+// 6: muestra en que estacion del ano nos encontramos dependiendo del valor de una variable mes
+
+mes= 'mayo'
+switch(mes){
+
+case 'enero':
+case'febrero':
+case'marzo':
+console.log('es invvierno')
+break
+
+case'abril':
+case'mayo':
+case'junio':
+console.log('es primavera')
+break
+
+case'julio':
+case'agosto':
+case'septiembre':
+console.log('es verano')
+break
+
+case'octubbre':
+case'noviembre':
+case'diciembre':
+console.log('es otono')
+break
+
+default:
+console.log('el mes no es valido')
+break
+}
+
+// 7: muestra el numero de dias que tiene un mes dependiendo de la variable del ejercicio anterior
+
+switch(mes){
+
+case 'enero':
+    console.log('el mes tiene 31 dias') 
+break
+
+case 'febrero':
+    console.log('el mes tiene 28 dias')
+break
+
+case 'marzo':
+    console.log('el mes tiene 31 dias')
+break
+
+case 'abril':
+    console.log('el mes tiene 30 dias')
+break
+
+case 'mayo':
+    console.log('el mes tiene 31 dias')
+break
+
+case 'junio':
+    console.log('el mes tiene 30 dias')
+break
+
+case 'julio':
+    console.log('el mes tiene 31 dias')
+break
+
+case 'agosto':
+    console.log('el mes tiene 31 dias')
+break
+
+case 'septiembre':
+    console.log('el mes tiene 30 dias')
+break
+
+case 'octubre':
+    console.log('el mes tiene 31 dias')
+break
+
+case 'noviembre':
+    console.log('el mes tiene 30 dias')
+break
+
+case 'diciembre':
+    console.log('el mes tiene 31 dias')
+break
+
+default:
+console.log('el mes no es valido')
+break
+}
+
+// 8: usa un switch para imprimir para imprimir un saludo diferente dependiendo del idioma
+
+let idioma= 'ingles'
+switch(idioma){
+
+    case'espanol':
+        console.log('hola')
+    break
+    case'ingles':
+        console.log('hello')
+    break
+    case'italiano':
+        console.log('ciao')
+    break
+    default:
+        console.log('no tenemos ese idioma en la base de datos')
+
+}
+
+// 9: usa un switch para hacer de nuevo el ejercicio 6
+
+// ya esta echo \_(^v^)_/
+
+// 10: usa un switch para hacer de nuevo el ejercicio 7
+
+// ya esta echo \_(^v^)_/
