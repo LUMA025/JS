46103ff4dbe765c26a4d631fb376cdca63d0acf9
LUMA025 <luismanuel.rincon24@gmail.com>
2025-04-03 16:54:31 -0400

    Delete Node directory

--- Node/app2.js
+++
@@ -1,5 +0,0 @@
-/* ---CONSOLE--- */
-
-console.log('Hola mundo') // Misma situacion que en curso de javascript, multiples maneras de interactuar con la consola y mostrar mensajes personalizados o incluso advertencias o provocar errores
-console.warn('Esto es una advertencia') // En este caso esto es una advertencia.
-console.error(new Error('Esto es un error')) // Y asi es como podemos hacer que un error se ocasione voluntariamente en la consolan, mostrando un mesaje personalizado del porque.
