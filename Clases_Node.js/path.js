20d31c52c6ce38f4f02588ac03b09b4f1c821ef7
LUMA025 <luismanuel.rincon24@gmail.com>
2025-04-04 16:51:38 -0400

    Delete Clases_Node.js directory

--- Clases_Node.js/path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-const filepath = path.join('/Users', 'LUMA', '/Desktop')
-
-console.log(filepath)
-console.log(path.basename(filepath))
-console.log(path.dirname(filepath))
-console.log(path.parse(filepath))
-console.log(path.resolve('dist'))
